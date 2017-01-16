033b895000b3f2bd5a5165e4d2e28fbe8052a42a
Xilong Fan <xfan@spokeo.com>
2017-01-16 19:29:44 +0000

    upd module

--- dynamic_dynamodb/statistics/table_metric_buffer.py
+++ dynamic_dynamodb/statistics/table_metric_buffer.py
@@ -16,27 +16,30 @@ class TableMetricBuffer:
 
         self.sampling_window_in_sec = sampling_window_in_sec
 
-    def check_and_calculate_weighted_read_units(self, updated_read_units):
-        throttle_events_delta = self.curr_throttled_reads_cnt - self.prev_throttled_reads_cnt
-        consumed_events_delta = self.curr_consuming_reads_cnt - self.prev_consuming_reads_cnt
-        if (0 < throttle_events_delta):
+    def check_and_calc_weighted_units_when_throttle(self, curr_throttled_cnt, prev_throttled_cnt,
+                                                          curr_consuming_cnt, prev_consuming_cnt,
+                                                          curr_provision_cnt, updated_units):
+        if (0 < curr_throttled_cnt):
+            throttle_events_delta = curr_throttled_cnt - prev_throttled_cnt
             throttle_events_delta_to_total_req_ratio = (
-                float(throttle_events_delta) / \
-                float(self.prev_throttled_reads_cnt + self.prev_consuming_reads_cnt))
-            consumed_events_delta_to_provi_cap_ratio = (
-                float(self.consumed_events_delta) / float(self.curr_provision_reads_cnt))
-            throttle_events_delta_ratio = (float(throttle_events_delta) / \
-                                           float(throttle_events_delta + consumed_events_delta))
-            current_units = self.curr_provision_reads_cnt / self.sampling_window_in_sec
-            target_read_units = int(
-                ((current_units * throttle_events_delta_ratio) * \
-                 (throttle_events_delta_to_total_req_ratio + 1.0)) + \
-                ((current_units * (1.0 - throttle_events_delta_ratio)) * \
-                 (consumed_events_delta_to_provi_cap_ratio + 1.0)))
+                float(throttle_events_delta) / float(prev_throttled_cnt + prev_consuming_cnt))
+            current_units = curr_provision_cnt / self.sampling_window_in_sec
+            target_units = int(current_units * (throttle_events_delta_to_total_req_ratio + 1.0))
+            if (target_units > updated_units): updated_units = target_units
 
-            if (target_read_units > updated_read_units):
-                updated_read_units = target_read_units
-        return updated_read_units
+        return updated_units
+
+    def check_and_calculate_weighted_read_units(self, updated_read_units):
+        return check_and_calc_weighted_units_when_throttle(
+            self.curr_throttled_reads_cnt, self.prev_throttled_reads_cnt,
+            self.curr_consuming_reads_cnt, self.prev_consuming_reads_cnt,
+            self.curr_provision_reads_cnt, updated_read_units)
+
+    def check_and_calculate_weighted_write_units(self, updated_write_units):
+        return check_and_calc_weighted_units_when_throttle(
+            self.curr_throttled_write_cnt, self.prev_throttled_write_cnt,
+            self.curr_consuming_write_cnt, self.prev_consuming_write_cnt,
+            self.curr_provision_write_cnt, updated_write_units)
 
     def to_dict(self):
         return {
